92a0869b39606108fd2040ad21307d47beac7a85
matheus8695 <0821matheus@gmail.com>
2024-07-24 11:05:45 -0300

    API-2: [validation rules] question required

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -35,3 +35,16 @@ test('after creating a new question I need to make sure that it creates on _draf
         'question' => 'Lorem ipsum jeremias?',
     ]);
 });
+
+describe('validation rules', function () {
+    test('question::required', function () {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        postJson(route('questions.store', []))
+            ->assertJsonValidationErrors([
+                'question' => 'required',
+            ]);
+    });
+});
