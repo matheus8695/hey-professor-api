dbe5f3bfe1cafd9cb8123210a90b990bd5a8be50
matheus8695 <0821matheus@gmail.com>
2024-07-24 16:33:29 -0300

    API-2: refactoring to use user relantionships instead of infere the user_id on the criation

--- app/Http/Controllers/Question/StoreController.php
+++ app/Http/Controllers/Question/StoreController.php
@@ -5,17 +5,16 @@ namespace App\Http\Controllers\Question;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Question\StoreRequest;
 use App\Http\Resources\QuestionResource;
-use App\Models\Question;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request): QuestionResource
     {
-        $question = Question::create([
-            'user_id'  => auth()->user()->id,
-            'question' => $request->question,
-            'status'   => 'draft',
-        ]);
+        $question = user()->questions()
+            ->create([
+                'question' => $request->question,
+                'status'   => 'draft',
+            ]);
 
         return QuestionResource::make($question);
     }
