c13c556b8c86e26dec15d5140f36d6d1daba0740
matheus8695 <0821matheus@gmail.com>
2024-08-01 11:02:33 -0300

    API-21: listing all draft quesntions related a looged user

--- tests/Feature/Question/MyQuestionsTest.php
+++ tests/Feature/Question/MyQuestionsTest.php
@@ -28,3 +28,27 @@ it('should list only questions that the logged user has been created::published'
         'name' => $anotherUserQuestion->name,
     ]);
 });
+
+it('should list only questions that the logged user has been created::draft', function () {
+    $user                = User::factory()->create();
+    $userQuestion        = Question::factory()->draft()->for($user, 'user')->create();
+    $anotherUserQuestion = Question::factory()->draft()->create();
+
+    Sanctum::actingAs($user);
+
+    $request = getJson(route('my-questions', ['status' => 'draft']))->assertOk();
+
+    $request->assertJsonFragment([
+        'id'         => $userQuestion->id,
+        'question'   => $userQuestion->question,
+        'status'     => $userQuestion->status,
+        'created_by' => [
+            'id'   => $userQuestion->user->id,
+            'name' => $userQuestion->user->name,
+        ],
+        'created_at' => $userQuestion->created_at->format('Y-m-d h:i:s'),
+        'updated_at' => $userQuestion->updated_at->format('Y-m-d h:i:s'),
+    ])->assertJsonMissing([
+        'name' => $anotherUserQuestion->name,
+    ]);
+});
