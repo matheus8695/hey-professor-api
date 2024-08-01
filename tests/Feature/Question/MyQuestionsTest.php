ddfcc514e3c1fd9f157e59b59c863a15db1b9960
matheus8695 <0821matheus@gmail.com>
2024-08-01 11:00:53 -0300

    API-21: listing all published quesntions related a looged user

---
+++ tests/Feature/Question/MyQuestionsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\{Question, User};
+use Laravel\Sanctum\Sanctum;
+
+use function Pest\Laravel\getJson;
+
+it('should list only questions that the logged user has been created::published', function () {
+    $user                = User::factory()->create();
+    $userQuestion        = Question::factory()->published()->for($user, 'user')->create();
+    $anotherUserQuestion = Question::factory()->published()->create();
+
+    Sanctum::actingAs($user);
+
+    $request = getJson(route('my-questions', ['status' => 'published']))->assertOk();
+
+    $request->assertJsonFragment([
+        'id'         => $userQuestion->id,
+        'question'   => $userQuestion->question,
+        'status'     => $userQuestion->status,
+        'created_by' => [
+            'id'   => $userQuestion->user->id,
+            'name' => $userQuestion->user->name,
+        ],
+        'created_at' => $userQuestion->created_at->format('Y-m-d h:i:s'),
+        'updated_at' => $userQuestion->updated_at->format('Y-m-d h:i:s'),
+    ])->assertJsonMissing([
+        'name' => $anotherUserQuestion->name,
+    ]);
+});
