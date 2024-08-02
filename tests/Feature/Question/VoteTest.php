ddc8d3a48826fed0b95046cff27b272571bf1882
matheus8695 <0821matheus@gmail.com>
2024-08-02 10:30:01 -0300

    API-17: should be able to unlike a question

--- tests/Feature/Question/VoteTest.php
+++ tests/Feature/Question/VoteTest.php
@@ -27,3 +27,26 @@ it('should be able to like a question', function () {
         'like'        => 1,
     ]);
 });
+
+it('should be able to unlike a question', function () {
+    $user = User::factory()->create();
+    Sanctum::actingAs($user);
+
+    $question = Question::factory()->published()->create();
+
+    postJson(
+        route('questions.vote', [
+            'question' => $question,
+            'vote'     => 'unlike',
+        ])
+    )->assertNoContent();
+
+    expect($question->votes)
+        ->toHaveCount(1);
+
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'user_id'     => $user->id,
+        'unlike'      => 1,
+    ]);
+});
